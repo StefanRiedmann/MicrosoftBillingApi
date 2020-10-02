6ffa0ba5f45d762f77f0da0ef2ab172e0175de12
Stefan Riedmann <stefan.riedmann@ciclosoftware.com>
2020-10-02 01:13:24 -0300

    BillingApiExample

---
+++ BillingApiExample/Controllers/BillingController.cs
@@ -0,0 +1,49 @@
+﻿using System.Threading.Tasks;
+using BillingApiSdk.Models;
+using BillingApiSdk.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillingApiExample.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BillingController : ControllerBase
+    {
+        private readonly IBillingApiService service;
+
+        public BillingController(IBillingApiService service)
+        {
+            this.service = service;
+        }
+
+        [HttpGet("[action]")]
+        public async Task<MicrosoftSubscription> ResolvePurchase([FromQuery] string billingToken)
+        {
+            var result = await service.ResolvePurchase(billingToken);
+            var subsription = await service.GetSubscription(result.SubscriptionId);
+            return subsription;
+        }
+
+        [HttpPost("[action]")]
+        public async Task ActivateSubscription([FromQuery] string billingToken)
+        {
+            var purchase = await service.ResolvePurchase(billingToken);
+            var subscription = await service.GetSubscription(purchase.SubscriptionId);
+            // ToDo: Before activing the subscription on Microsoft side, you
+            // have to configure your users account accordingly
+            // ...
+
+            await service.ActivateSubscription(subscription.SubscriptionId);
+        }
+
+        [HttpPost("[action]")]
+        public async Task SubscriptionWebhook([FromBody] MicrosoftWebhookPayload payload)
+        {
+            // ToDo: Perform the changes described in the payload on your user's account
+            // Alternative: Get the full operation for more details
+            var operation = await service.GetOperationStatus(payload.SubscriptionId, payload.OperationId);
+
+            await service.UpdateOperationStatus(payload.SubscriptionId, payload.OperationId, "Success");
+        }
+    }
+}
---
+++ BillingApiExample/Startup.cs
@@ -0,0 +1,64 @@
+using BillingApiSdk.Configuration;
+using BillingApiSdk.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json;
+
+namespace BillingApiExample
+{
+    public class Startup
+    {
+        private BillingApiConfiguration billingApiConfig;
+
+        public Startup(IWebHostEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", false, true)
+                .AddJsonFile("appsettings.development.json", true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            billingApiConfig = new BillingApiConfiguration();
+            Configuration.GetSection("BillingApiConfiguration").Bind(billingApiConfig);
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .AddNewtonsoftJson(o =>
+                {
+                    o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                    o.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
+                });
+            services.AddScoped<IBillingApiService>(s => new BillingApiService(billingApiConfig));
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
